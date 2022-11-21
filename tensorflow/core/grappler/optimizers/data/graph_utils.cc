6ba33a06f13db51ab0e4e37e322e832635cf658b
omrazCZ <oto.mraz@gmail.com>
2022-11-21 10:27:20 +0100

    Add extra logging

--- tensorflow/core/grappler/optimizers/data/graph_utils.cc
+++ tensorflow/core/grappler/optimizers/data/graph_utils.cc
@@ -278,7 +278,9 @@ std::vector<int> FindAllGraphNodesWithOp(const string& op,
 
 NodeDef* GetInputNode(const NodeDef& node, const MutableGraphView& graph) {
   if (node.input_size() == 0) return nullptr;
+  VLOG(0) << "Checked input size";
   MutableGraphView::InputPort input_port = graph.GetInputPort(node.name(), 0);
+  VLOG(0) << "Got input port";
   return graph.GetRegularFanin(input_port).node;
 }
 
